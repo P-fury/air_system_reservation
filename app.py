525ac62156e4658c994e674cb9fcb06c4c3d9003
Piotr Plucinski <plutocamera@gmail.com>
2024-10-10 21:33:58 +0200

    seat checker

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+from pprint import pprint as pp
+
+
 class Flight:
     def __init__(self, flight_number, airplane):
         self.airplane = airplane
         self.flight_number = flight_number
+        rows, seats = self.airplane.get_seating_plan()
+        self.seating_plan = [{letter: None for letter in seats} for _ in rows]
 
     def get_airline(self):
         return self.flight_number[:2]
@@ -12,9 +17,26 @@ class Flight:
     def get_model(self):
         return self.airplane.get_airplane_model()
 
-    def get_place(self):
-        return self.airplane.get_seating_plan()
+    def allocate_passenger(self, passenger="Lech K", seat="12C"):
+        row, letter = self._parse_seat(seat)
 
+    def _parse_seat(self, seat):
+        rows, seats = self.airplane.get_seating_plan()
+
+        letter = seat[-1]
+
+        if letter not in seats:
+            raise ValueError(f"Invalid seat letter: {letter}")
+
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError(f"Invalid row number: {row_text}")
+        if row not in rows:
+            raise ValueError(f"Row number is out of range: {row}")
+
+        return row,letter
 
 class AirPlane:
     def get_seats_no(self):
@@ -48,7 +70,6 @@ boeing = Boeing737Max()
 f = Flight('LO127', airbus)
 
 # print(f.get_place())
-#
 # print(f.get_model())
 # print(f.get_airline())
 # print(f.get_number())
@@ -56,3 +77,7 @@ f = Flight('LO127', airbus)
 
 print(boeing.get_seats_no())
 print(airbus.get_seats_no())
+
+pp(f.seating_plan)
+
+f.allocate_passenger(passenger='Jak Srak', seat='12A')
